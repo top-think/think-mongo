f3cac5f2abf78b38182f505eaf3997db3de2398c
Johnnie <ichenq@outlook.com>
2019-04-14 10:35:59 +0400

    修复“未定义数组下标: 1”
    See issue 90 https://github.com/top-think/think-mongo/issues/90

--- src/Builder.php
+++ src/Builder.php
@@ -107,7 +107,7 @@ class Builder
 
             if (is_object($val)) {
                 $result[$item] = $val;
-            } elseif (isset($val[0]) && 'exp' == $val[0]) {
+            } elseif (isset($val[0]) && 'exp' === $val[0]) {
                 $result[$item] = $val[1];
             } elseif (is_null($val)) {
                 $result[$item] = 'NULL';
