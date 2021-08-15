7be504a5f9d837d3f6be81b0631d0f9eacb6b4f2
yzw <316193839@qq.com>
2021-08-15 17:40:28 +0800

    fix ci:修复了count报错 BSON field 'count.query' is the wrong type 'array', expected type 'object' 的错误
    fix ci:修复了count报错 BSON field 'count.query' is the wrong type 'array', expected type 'object' 的错误

--- src/Builder.php
+++ src/Builder.php
@@ -205,7 +205,7 @@ class Builder
             list($field, $condition) = $options['soft_delete'];
             $filter['$and'][]        = $this->parseWhereItem($query, $field, $condition);
         }
-        if (empty($filter)) { // 返回空对象
+        if (empty($filter)) {
             return new \stdClass();
         }
         return $filter;
